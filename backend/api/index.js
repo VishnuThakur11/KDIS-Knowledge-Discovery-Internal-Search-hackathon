4fbed8c821f12674769bd0291d96ec11dc4840a1
vishnuthakur11 <vishnuhajam01@gmail.com>
2025-11-21 11:06:58 +0530

    iii

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,5 +1,37 @@
-// api/index.js
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import { connectDB } from "../src/config/mongo.js";
+import uploadRoutes from "../src/routes/upload.js";
+import searchRoutes from "../src/routes/search.js";
+import authRoutes from "../src/routes/auth.js";
+import file from "../src/routes/file.js";
+import { errorHandler } from "../src/middleware/errorHandler.js";
 
-import app from "../src/index.js";
+dotenv.config();
+connectDB();
 
+const app = express();
+
+// CORS
+app.use(cors({
+  origin: "https://internal-search-app-vishnu-hajam.vercel.app",
+  credentials: true
+}));
+
+app.use(express.json());
+
+// Routes
+app.use("/api/auth", authRoutes);
+app.use("/api/upload", uploadRoutes);
+app.use("/api/search", searchRoutes);
+app.use("/api/files", file);
+
+app.get("/", (req, res) => {
+  res.json({ status: "backend OK" });
+});
+
+app.use(errorHandler);
+
+// ❗ NO app.listen() here
 export default app;
